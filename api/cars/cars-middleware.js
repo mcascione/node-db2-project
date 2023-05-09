efbaa3a330d49138cacebea3553107e9de0de6f1
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-09 14:12:21 -0700

    get by id router and wired up with middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,7 +1,19 @@
-const db = require("../../data/db-config");
+const Car = require("./cars-model");
 
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Car.getById(req.params.id);
+    if (!car) {
+      res.status(404).json({
+        message: `car with id ${req.params.id} is not found`,
+      });
+    } else {
+      req.car = car;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 const checkCarPayload = (req, res, next) => {
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -21,6 +21,24 @@ router.get("/", (req, res, next) => {
     });
 });
 
+router.get("/:id", checkCarId, (req, res, next) => {
+  Car.getById(req.params.id)
+    .then((car) => {
+      res.json(car);
+    })
+    .catch(next);
+});
+
+// router.post(
+//   "/",
+//   checkCarPayload,
+//   checkVinNumberUnique,
+//   checkVinNumberValid,
+//   (req, res, next) => {
+//     Car.create(req.body).then().catch(next);
+//   }
+// );
+
 //eslint-disable-next-line
 router.use((err, req, res, next) => {
   res.status(err.status || 500).json({
