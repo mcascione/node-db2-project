826a4e385293d619a3bbf1c23f808bab77911cea
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-09 13:35:29 -0700

    wired up server with api

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const Car = require("./cars-model");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+} = require("../cars/cars-middleware");
+
+const router = express.Router();
+
 // DO YOUR MAGIC
+
+module.exports = router;
